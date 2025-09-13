52d971d4330a9b3d8c7e226f1cd375ae239eb9a8
ismailmoufid47 <ismailmoufid47@gmail.com>
2025-09-13 12:53:28 +0100

    changed bsp's main message

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -9,19 +9,19 @@ int main()
 	Point c(0, 10);
 
 	Point inside(2, 2);
-	std::cout << "Is ( 2,  2) inside triangle ABC(A(0,0), B(10,0), C(0,10))? ";
+	std::cout << "Is ( 2,  2) inside triangle A(0,0), B(10,0), C(0,10)? ";
 	std::cout << (bsp(a, b, c, inside) ? "Yes" : "No") << std::endl;
 
 	Point outside(10, 10);
-	std::cout << "Is (10, 10) inside triangle ABC(A(0,0), B(10,0), C(0,10))? ";
+	std::cout << "Is (10, 10) inside triangle A(0,0), B(10,0), C(0,10)? ";
 	std::cout << (bsp(a, b, c, outside) ? "Yes" : "No") << std::endl;
 
 	Point vertexA(0, 0);
-	std::cout << "Is ( 0,  0) inside triangle ABC(A(0,0), B(10,0), C(0,10))? ";
+	std::cout << "Is ( 0,  0) inside triangle A(0,0), B(10,0), C(0,10)? ";
 	std::cout << (bsp(a, b, c, vertexA) ? "Yes" : "No") << std::endl;
 
 	Point onEdge(5, 0);
-	std::cout << "Is ( 5,  0) inside triangle ABC(A(0,0), B(10,0), C(0,10))? ";
+	std::cout << "Is ( 5,  0) inside triangle A(0,0), B(10,0), C(0,10)? ";
 	std::cout << (bsp(a, b, c, onEdge) ? "Yes" : "No") << std::endl;
 
 }
